db5aae1f6e5ee5cdcb23d0662c0810287bcddc8a
Bobby <bobbykumar706584@gmail.com>
2022-02-22 20:05:25 +0530

    remove material ui

--- src/components/hello-name/HelloName.js
+++ src/components/hello-name/HelloName.js
@@ -1,10 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withStyles } from "@mui/styles";
-import { Typography } from "@mui/material";
 import { greetName } from "../../core";
 
-function HelloName({ classes, name, style }) {
+function HelloName({ name }) {
   const greeting = greetName({ name });
   return { greeting };
 }
